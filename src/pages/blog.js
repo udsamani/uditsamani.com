1b802069146f2e5701f9c50b52a3050f2dca630a
udsamani <uditsamani@outlook.com>
2019-03-19 06:48:13 -0500

    Blog Page Done

---
+++ src/pages/blog.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../layouts'
+import PostListing from '../components/PostListing'
+import config from '../../data/SiteConfig'
+import kebabCase from 'lodash.kebabcase'
+import _ from 'lodash'
+
+class BlogPage extends Component {
+    state = {
+        posts: this.props.data.posts.edges
+    }
+
+    render() {
+        const { posts } = this.state
+        const categories = this.props.data.categories.group.filter(
+            category => category.fieldValue !== 'Popular'
+        )
+
+        return (
+            <Layout>
+                <Helmet title = {`Articles - ${config.siteTitle}`}/>
+                <div className='container'>
+                    <h1>Articles</h1>
+                    <div className='tag-container articles-page-tags'>
+                        {categories.map(
+                            category => (
+                                <Link to={`/categories/${kebabCase(category.fieldValue)}`} key={category.fieldValue}>
+                                    <span key={category.fieldValue}>
+                                        {category.fieldValue} <strong className='count'>{category.totalCount}</strong>
+                                    </span>
+                                </Link>
+                            )
+                        )}
+                    </div>
+                    <PostListing excerpt postEdges={posts}/>
+                </div>
+            </Layout>
+        )
+    }
+}
+
+export default BlogPage
+
+export const pageQuery = graphql `
+    query BlogQuery {
+        categories: allMarkdownRemark(limit:2000) {
+            group(field:frontmatter___categories) {
+                fieldValue
+                totalCount
+            }
+        }
+        posts: allMarkdownRemark(limit:2000, sort:{fields:frontmatter___date, order:DESC}){
+           edges{
+             node{
+               frontmatter {
+                 title
+                 slug
+                 date
+                 tags
+                 thumbnail {
+                   childImageSharp {
+                     fixed(width:150, height:150) {
+                       tracedSVG
+                     }
+                   }
+                 }
+                 date
+                 template
+               }
+             }
+           }
+       }
+
+
+    }
+`
