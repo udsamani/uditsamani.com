7caebcf991e09922c6a8f6d872099841de175aec
Udit Samani <udit.samani@thoughtspot.com>
2020-05-22 16:59:00 -0700

    UserInfo Component added

---
+++ src/components/UserInfo.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import udit from '../images/udit.jpg'
+
+export default class UserInfo extends Component {
+  render() {
+    return (
+      <aside className="note">
+        <div className="container note-container">
+          <div className="flex-author">
+            <div className="flex-avatar">
+              <img className="avatar" src={udit} alt="Tania Rascia" />
+            </div>
+            <div>
+              <h3>Author</h3>
+              <p>
+                Hey, I’m Udit, a Software engineer. I write about what I know will help
+                the upcoming generation of CS engineers. Life is about giving what you have.
+              </p>
+            </div>
+          </div>
+        </div>
+      </aside>
+    )
+  }
+}>
