237a48705588d927af9d7862fe695ef0a1df3fca
Udit Samani <udit.samani@thoughtspot.com>
2020-05-22 02:56:35 -0700

    Navigation Component Refactored

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 import ThemeContext from '../context/ThemeContext'
 import hp from '../images/hp.svg'
-import cv from '../images/cv.svg'
 import sun from '../images/sun.svg'
 import moon from '../images/moon.svg'
 
@@ -32,56 +31,52 @@ class Navigation extends React.Component {
       }
 
       //Component life cycle function to remove scroll even listener
-      /*
       componentWillUmount() {
-          window.removeEventListener('scroll', this.navOnScroll)
+          window.removeEventListener('scroll', this.navigationOnScroll)
       }
-      */
 
       //Rendering JSX
       render() {
           const { scrolled } = this.state
           const { menuLinks } = this.props
-          const { dark, toggleDark, setFound} = this.context
+          const theme = this.context
 
           return(
-              <ThemeContext.Consumer>
-                {value => (
-                      <nav className = {scrolled ? 'nav scroll' : 'nav'}>
-                        <div className = 'nav-container'>
-                            <div className = 'brand'>
-                                <Link to='/'>
-                                    <img src={hp} className='favicon'/>{' '}
-                                    <span className='text'>Udit Samani</span>
-                                </Link>
-                            </div>
-                            <div className='links'>
-                                {menuLinks.map(link => (
-                                    <Link key={link.name} to={link.link}>
-                                        {link.name}
-                                    </Link>
-                                ))}
-                            </div>
-                            <div className='cta'>
-                                <button className='dark-switcher' onClick={value.toggleDark}>
-                                    {value.dark ? (
-                                        <span>
-                                            <img src={sun} className='theme-icon'/>
-                                        </span>
-                                    ) : (
-                                        <span>
-                                            <img src={moon} className='theme-icon'/>
-                                        </span>
-                                    )}
-                                </button>
-                                <a className='donate-button' href='https://drive.google.com/file/d/1zt_qRZp-i2eeY0Ps_LXlKXc0sDiTwPXE/view?usp=sharing' target ='_blank'>
-                                    <img src={cv} className='coffee-icon'/>
-                                </a>
-                            </div>
-                        </div>
-                      </nav>
-                )}
-            </ThemeContext.Consumer>
+            <nav className = {scrolled ? 'nav scroll' : 'nav'}>
+              <div className = 'nav-container'>
+                  <div className = 'brand'>
+                      <Link to='/'>
+                          <img src={hp} className='favicon'/>{' '}
+                          <span className='text'>Udit Samani</span>
+                      </Link>
+                  </div>
+                  <div className='links'>
+                      {menuLinks.map(link => (
+                          <Link key={link.name} to={link.link} activeClassName="active">
+                              {link.name}
+                          </Link>
+                      ))}
+                  </div>
+                  <div className='cta'>
+                      <button 
+                        className='dark-switcher' 
+                        onClick={theme.toggleDark}
+                        aria-label="Toggle Dark Mode."
+                        title="Toggle Dark Mode"
+                      >
+                        {theme.dark ? (
+                            <span>
+                                <img src={sun} className='theme-icon'/>
+                            </span>
+                        ) : (
+                            <span>
+                                <img src={moon} className='theme-icon'/>
+                            </span>
+                        )}
+                      </button>
+                  </div>
+              </div>
+            </nav>
           )
       }
 }
